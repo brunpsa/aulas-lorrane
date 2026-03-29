07de94d455009999ee8f4976b25e4e62fc2d6178
Bruno <bruno@Brunos-MacBook-Air.local>
2026-03-29 07:47:22 -0300

    .

--- src/Ex5.java
+++ src/Ex5.java
@@ -2,10 +2,9 @@ public class Ex5 {
     public static void main(String[] args) {
 
         int numero = 3;
-        int resultado;
 
         for (int i = 1; i <= 10; i++) {
-            resultado = numero * i;
+            int resultado = numero * i;
 
             if (resultado % 2 == 0) {
                 System.out.println(numero + " x " + i + " = " + resultado + " --> Par");
